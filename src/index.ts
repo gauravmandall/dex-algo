47415b9d9184d88588a4e947e217d91a3475a62c
gauravmandall <mandalgaurav550@gmail.com>
2024-05-19 23:51:22 +0530

    added Structure

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+ 
+const app = express();
+const ETH_BALANCE = 200;
+const USDT_BALANCE = 600000;
+
+
+app.post("/add-liquidity", (req, res) => {
+    
+})
+
+app.post("/buy-asset", (req, res) => {
+
+})
+
+app.post("/sell-asset", (req, res) => {
+
+})
+
+app.post("/quote", (req, res) => {
+
+})
